82f7de83961805f89a140ae97acd98af391b55f6
Tomasz Kłos <xilent98@wp.pl>
2020-08-29 18:09:11 +0200

    DrinkWater version 1.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,20 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀')
 
-const toggle  = document.getElementById("toggle");
-const content = document.getElementById("content");
+const info = document.querySelector(".info--js");
+const statistics = document.querySelector(".statistics--js");
+const settings = document.querySelector(".settings--js");
 const glassCounter = document.querySelector(".glass__counter--js");
 const buttonAdd = document.querySelector(".button--js");
 const buttonRemove = document.querySelector(".button-remove--js");
 const key = new Date().toLocaleString().slice(0, 10);
+const hideInfo = document.querySelector(".x--js");
+const hideStatistics = document.querySelector(".x--statistics");
+const hideSettings = document.querySelector(".x--settings");
+const infoOpen = document.querySelector(".info");
+const statisticsOpen = document.querySelector(".statistics");
+const settingsOpen = document.querySelector(".settings");
+const done = document.querySelector(".alert--js");
 
 let currentGlassCounter = 0;
 
@@ -23,14 +31,11 @@ if (localeStorageValue) {
     currentGlassCounter = localeStorageValue;
 } else {
     localStorage.setItem(key, 0);
-};
+}
 
-currentGlassCounter = localeStorageValue;
+glassCounter.innerHTML = currentGlassCounter;
 
-
-toggle.addEventListener("click", function() {
-  content.classList.toggle("show");
-});
+// Button add and remove
 
 buttonAdd.addEventListener('click', () => {
     currentGlassCounter++;
@@ -46,3 +51,37 @@ buttonRemove.addEventListener('click', () => {
     glassCounter.innerHTML = currentGlassCounter;
     localStorage.setItem(key, currentGlassCounter);
 });
+
+// Menu button
+
+info.addEventListener("click", () => {
+    infoOpen.classList.toggle("info--show");
+
+});
+
+hideInfo.addEventListener('click', () => {
+    infoOpen.classList.toggle("info--show");
+});
+
+statistics.addEventListener("click", () => {
+    statisticsOpen.classList.toggle("statistics--show");
+
+});
+
+hideStatistics.addEventListener('click', () => {
+    statisticsOpen.classList.toggle("statistics--show");
+});
+
+settings.addEventListener("click", () => {
+    settingsOpen.classList.toggle("settings--show");
+
+});
+
+hideSettings.addEventListener('click', () => {
+    settingsOpen.classList.toggle("settings--show");
+});
+
+
+// test
+
+
